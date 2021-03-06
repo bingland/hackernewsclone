55f54a98002f0dac8505841e66bd09e08013d5fd
Brayden England <bray.patriot@gmail.com>
2021-03-05 19:58:56 -0700

    Get and sort top stories from API

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,55 @@
 <script>
-	
+	import { onMount } from 'svelte'
+
+	const topStoriesURL = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+
+	let resultsReady = false
+	let topStories = []
+
+	const getByID = async (id) => {
+		fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+				topStories = [...topStories, data];
+			})
+	}
+
+	onMount(async () => {
+		fetch(topStoriesURL)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+				for (let i = 0; i < 30; i++) {
+					getByID(data[i])
+				}
+			})
+	})
+
+	// if array length is 30, sort the array by points
+	$: if (topStories && topStories.length === 30) {
+		topStories.sort((a, b) => { return a.score > b.score ? -1 : 1 })
+		resultsReady = true
+	}
 </script>
 
 <main>
-	<h1>Hello world!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hackernews Clone</h1>
+	{#if resultsReady}
+		{#each topStories as story}
+			<p>{story.title}</p>
+		{/each}
+	{/if}
 </main>
 
 <style>
 	main {
-		text-align: center;
+		text-align: left;
 		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
