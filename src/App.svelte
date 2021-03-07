7f35b0ae47be0274b4030271f36d50abe2f09586
Brayden England <bray.patriot@gmail.com>
2021-03-06 17:47:07 -0700

    Styling to mimic the original hacker news

--- src/App.svelte
+++ src/App.svelte
@@ -26,30 +26,89 @@
 			})
 	})
 
+	// return base URL for output
+	const getHostname = (url) => {
+		if (url !== undefined)
+			return new URL(url).hostname
+		return ''
+	}
+
+
 	// if array length is 30, sort the array by points
 	$: if (topStories && topStories.length === 30) {
 		topStories.sort((a, b) => { return a.score > b.score ? -1 : 1 })
+		topStories.forEach(story => story.baseurl = getHostname(story.url))
 		resultsReady = true
 	}
 </script>
 
 <main>
-	<h1>Hackernews Clone</h1>
+	<nav>Hacker News Clone</nav>
 	{#if resultsReady}
-		{#each topStories as story}
-			<p>{story.title}</p>
+		{#each topStories as story, i}
+			<div class="story">
+				<div class="line1">
+					<div class="rank">#{i + 1}</div> 
+					<a class="storyTitle" href="{story.url}">{story.title}</a> 
+					<a class="storyUrl" href="{story.url}">({story.url ? story.baseurl : 'No URL returned'})</a>
+				</div>
+				<div class="line2">{story.score} points by {story.by}</div>
+			</div>
 		{/each}
 	{/if}
 </main>
 
 <style>
 	main {
-		text-align: left;
-		padding: 1em;
+		font-family: 'Source Sans Pro', sans-serif;
+		max-width: 1200px;
+		margin: 5px auto;
+		background-color: #F2F2F0;
 	}
 
-	h1 {
-		font-size: 4em;
-		font-weight: 100;
+	nav {
+		font-size: 16px;
+		background-color: #F25C05;
+		font-weight: bold;
+		padding: 5px;
+		margin-bottom: 5px;
+	}
+
+	.story {
+		padding: 5px;
+		padding-left: 10px;
+	}
+
+	.line1 {
+		display: flex;
+		position: relative;
+	}
+	.rank {
+		color: rgb(78, 78, 78);
+		width: 29px;
+	}
+	.storyTitle {
+		color: black;
+		text-decoration: none;
+		cursor: pointer;
+	}
+	.storyUrl {
+		color: rgb(78, 78, 78);
+		padding-left: 4px;
+		font-size: 14px;
+		position: relative;
+		bottom: -2px;
+		cursor: pointer;
+		text-decoration: none;
+	}
+	.storyUrl:hover {
+		text-decoration: underline;
+	}
+
+	.line2 {
+		font-size: 13px;
+		padding-left: 29px;
+		padding-top: 3px;
+		color: rgb(78, 78, 78);
 	}
 </style>
